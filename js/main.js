7714b856bf8640069023dd0a5c85b9768f4bc1d8
Michelle Hartley <mdhcodes@gmail.com>
2016-06-05 21:53:02 -0400

    Refactor code and update the website

---
+++ js/main.js
@@ -0,0 +1,47 @@
+// Show and Hide Navigation Menu
+
+var showHideMenu = function() {
+
+    var menuButton = document.getElementById("menu-button");
+    var navMenu = document.getElementById("nav-menu");
+    var menuIsDisplayed = false;
+
+    menuButton.onclick = function() {
+
+        if(menuIsDisplayed) {
+
+            navMenu.style.display = "none";
+            menuIsDisplayed = false;
+
+        } else {
+
+            navMenu.style.display = "block";
+            menuButton.style.marginBottom = "1em";
+            menuIsDisplayed = true;
+
+        }
+    };
+
+    navMenu.onclick = function() {
+
+        navMenu.style.display = "none";
+        menuIsDisplayed = false;
+
+    };
+
+};
+
+showHideMenu();
+
+
+//  Highlight .selected #nav-menu link
+
+
+$(document).ready(function() {
+    $('#nav-menu li a').click(function() {
+        $('li a').removeClass("selected");
+        $(this).addClass("selected");
+    });
+});
+
+
