ac23f58b7f87e33560b6e96e178dabb8a56340dd
Michelle Hartley <mdhcodes@gmail.com>
2016-07-29 17:31:52 -0400

    Remove slider and front-end template

--- js/main.js
+++ js/main.js
@@ -1,52 +1,32 @@
 // Show and Hide Navigation Menu
 
 var showHideMenu = function() {
-
     var menuButton = document.getElementById("menu-button");
     var navMenu = document.getElementById("nav-menu");
     var nav = document.getElementById("nav");
     var header = document.getElementById("header");
     var menuIsDisplayed = false;
-
+    
     menuButton.onclick = function() {
-
         if(menuIsDisplayed) {
-
             navMenu.style.display = "none";
             nav.style.borderTop = "none";
             header.style.paddingBottom = "0";
             menuIsDisplayed = false;
-
         } else {
-
             navMenu.style.display = "block";
             nav.style.borderTop = "thin solid #baad99";
             header.style.paddingBottom = "1em";
             menuIsDisplayed = true;
-
         }
     };
 
     navMenu.onclick = function() {
-
         navMenu.style.display = "none";
         nav.style.borderTop = "none";
         header.style.paddingBottom = "0";
         menuIsDisplayed = false;
-
     };
-
 };
 
 showHideMenu();
-
-
-//  Highlight .selected #nav-menu link
-
-
-$(document).ready(function() {
-    $('#nav-menu li a').click(function() {
-        $('li a').removeClass("selected");
-        $(this).addClass("selected");
-    });
-});
